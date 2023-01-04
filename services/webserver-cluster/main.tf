9213a4cd37f3342b02abfad10e9b058ea2eb4610
Geraud <geraudkameni@gmail.com>
2023-01-04 17:04:34 +0100

    First release of webserver-cluster module

---
+++ services/webserver-cluster/main.tf
@@ -0,0 +1,200 @@
+provider "aws" {
+  region = "us-east-1"
+}
+data "aws_vpc" "default" {
+  default = true
+}
+
+#### Variables 
+variable "server_port" {
+  description = "The port the server will use to connect to HTTP requests"
+  type        = number
+  default     = 8080
+  
+}
+
+locals {
+  http_port    = 80
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
+}
+
+resource "aws_security_group" "http" {
+  name = "${var.cluster_name}-http"
+
+  ingress  {
+    description = "test"
+    from_port = var.server_port
+    to_port = var.server_port
+    protocol    = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  } 
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "terraform_remote_state" "db" {
+  backend = "s3"
+
+  config = {
+    bucket = var.db_remote_state_bucket
+    key    = var.db_remote_state_key
+    region = "us-east-1"
+  }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-04505e74c0741db8d"
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.http.id]
+  
+  user_data = templatefile("${path.module}/user-data.sh", {
+    server_port = var.server_port
+    db_address  = data.terraform_remote_state.db.outputs.address
+    db_port     = data.terraform_remote_state.db.outputs.port
+  })
+  
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration  = aws_launch_configuration.example.name
+  vpc_zone_identifier   = data.aws_subnets.default.ids
+
+  target_group_arns = [aws_lb_target_group.tg.arn]
+  health_check_type = "ELB"
+
+  min_size = var.min_size
+  max_size = var.max_size
+
+  tag {
+    key                 = "name"
+    value               = var.cluster_name
+    propagate_at_launch = true
+  }
+  depends_on = [
+    aws_lb_target_group.tg
+ ]
+}
+resource "aws_lb" "lb" {
+  name               = "${var.cluster_name}-lb"
+  load_balancer_type = "application"
+  subnets            = data.aws_subnets.default.ids
+  security_groups    = [aws_security_group.alb.id]
+  
+}
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = local.http_port
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code = 404
+    }
+  }
+}
+
+# resource "aws_security_group" "alb" {
+#  name = "${var.cluster_name}-alb"
+ 
+#  # Allow inbound HTTP requests
+#  ingress {
+#     from_port   = local.http_port
+#     to_port     = local.http_port
+#     protocol    = local.tcp_protocol
+#     cidr_blocks = local.all_ips
+#   }
+
+#   # Allow all outbound requests
+#   egress {
+#     from_port   = local.any_port
+#     to_port     = local.any_port
+#     protocol    = local.any_protocol
+#     cidr_blocks = local.all_ips
+#   }
+# }
+
+resource "aws_security_group" "alb" {
+  name = "${var.cluster_name}-alb"
+}
+
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type              = "ingress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.http_port
+  to_port     = local.http_port
+  protocol    = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type              = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port   = local.any_port
+  to_port     = local.any_port
+  protocol    = local.any_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_lb_target_group" "tg" {
+  name        = "${var.cluster_name}-tg"
+  port        = var.server_port
+  protocol    = "HTTP"
+  vpc_id      = data.aws_vpc.default.id
+  
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+}
+
+resource "aws_lb_listener_rule" "http-rule" {
+listener_arn = aws_lb_listener.http.arn
+priority           =  100
+  
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+  
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+# terraform {
+#   backend "s3" {
+#     # Replace this with your bucket name!
+#     bucket         = "terraform-up-and-running-state-geraud-2023-01"
+#     key            = "stage/services/webserver-cluster/terraform.tfstate"
+#     region         = "us-east-1"
+
+#     # Replace this with your DynamoDB table name!
+#     dynamodb_table = "terraform-up-and-running-locks"
+#     encrypt        = true
+#   }
+# }
+
